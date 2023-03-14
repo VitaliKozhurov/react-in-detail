9033055894fe627e435e4340d6914b08e356b5cd
VitaliKozhurov <vitalik24021992@gmail.com>
2023-03-14 17:30:08 +0300

    component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // описываем логику компоненты
+
+
+  // возвращаем JSX разметку
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      App component
+      <Rating/>
+      <Accordion/>
     </div>
   );
+}
+
+function Rating (){
+  return (
+    <>
+   {Array(5).fill('').map(el=><Star/>)}
+    </>
+  )
+}
+
+function Accordion (){
+
+  return (
+    <>
+    <h3>Menu</h3>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+    </>
+  )
+}
+function Star (){
+
+  return (
+    <div>star</div>
+  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -14,7 +14,3 @@ root.render(
 );
 
 reportWebVitals();
-
-const arr1: Array<string | number> = [123, 'asd', 'zxc'];
-
-console.log(arr1);
